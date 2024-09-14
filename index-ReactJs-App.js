bd0f53edf1b043ae8fc3ae1a3a086419290eeda8
Pragati <pragatikhard@gmail.com>
2024-09-14 22:48:38 +0530

    Basics

---
+++ index-ReactJs-App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1", { id: "heading", xyz: "xyz" }, "Hello World From React !    // External File")
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(heading)
