d133ffe065a0b664165bc527b30e27ccc7c23cbb
Pragati <pragatikhard@gmail.com>
2024-09-15 23:34:03 +0530

    Basics | Nested Structure | RootRender | When we use npm import the React

--- index-ReactJs-App.js
+++ index-ReactJs-App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 // const heading = React.createElement("h1", { id: "heading", xyz: "xyz" }, "Hello World From React !    // External File")
 // const root = ReactDOM.createRoot(document.getElementById("root"))
 // root.render(heading)
@@ -63,7 +66,7 @@
 const parent = React.createElement("div", { id: "parent" },
     [
         React.createElement("div", { id: "child" }),
-        [React.createElement("h1", {}, "I'm h1 tag"),
+        [React.createElement("h1", {}, "I'm h1 tag from React"),
         React.createElement("h2", {}, "I'm h2 tag")],
         React.createElement("div", { id: "child2" }),
         [React.createElement("h1", {}, "I'm h1 tag"),
