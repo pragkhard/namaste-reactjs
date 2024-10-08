7476b3399782436eb48cff3223cd1594e8dc3ec3
Pragati <pragatikhard@gmail.com>
2024-10-08 23:29:31 +0530

    Components | Header

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+const Header = () => {
+    return (
+        <div className="header">
+            <div className='logo-container'>
+                <img className="logo" src='https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png' />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact us</li>
+                    <li>Card</li>
+                </ul>
+
+            </div>
+        </div>
+
+    )
+}
+
+export default Header;
