6a84b64aeaa5c991eeded80bb940722c8639d96f
Pragati <pragatikhard@gmail.com>
2024-10-10 14:47:32 +0530

    Components | RestaurantCards

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,3 +1,6 @@
+// import the name import 
+import { CDN_URL } from '../utils/constants';
+
 
 const styleCard = {
     backgroundColor: "#f0f0f0"
@@ -10,7 +13,7 @@ const RestaurantCard = (props) => {
     return (
         <div className="res-card" style={styleCard}>
             <img className="res-logo" src={
-                "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + cloudinaryImageId
+                CDN_URL + cloudinaryImageId
             } />
             <h3>{name}</h3>
             <h4>{cuisines.join(",")}</h4>
