d7822617e469d4b799a9a5d1118e03fef788f41d
Pragati <pragatikhard@gmail.com>
2024-10-10 14:46:13 +0530

    Swiggy App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Header from './components/Header';
+// Named Import 
+import {Header} from './components/Header';
+// Default Import 
 import Body from './components/Body';
-
-
 
 
 const AppLayout = () => {
