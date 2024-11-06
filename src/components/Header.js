1923871bd21043a8d760dbc8b05a285607318c0d
Pragati <pragatikhard@gmail.com>
2024-11-06 19:03:02 +0530

    Components | Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { LOGO_URL } from '../utils/constants';
+import { Link } from 'react-router-dom';
 
 // Named Export 
 export const Header = () => {
     const [btnNameReact, setBtnNameReact] = useState("Login")
     console.log("Header Render")
+
+    // If no dependency array => useEffect is called on every render
+    // If dependency array is empty = [] => useEffect is called on initial render (just once)
+
+    useEffect(() => {
+        console.log("useEffect Called")
+    }, [btnNameReact]);
+
     return (
         <div className="header">
             <div className='logo-container'>
@@ -12,9 +21,20 @@ export const Header = () => {
             </div>
             <div className="nav-items">
                 <ul>
-                    <li>Home</li>
-                    <li>About us</li>
-                    <li>Contact us</li>
+                    <li>
+                        <Link to="/">
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/about">
+                            About us
+                        </Link>
+
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact us</Link>
+                    </li>
                     <li>Card</li>
                     <button className='Login' onClick={() => {
                         btnNameReact == "Login" ? setBtnNameReact("Logout") : setBtnNameReact("Login")
