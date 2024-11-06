4e246be88c023f94fee80b3c0a28a05f01985b0e
Pragati <pragatikhard@gmail.com>
2024-11-06 19:00:25 +0530

    Swiggy App

--- src/App.js
+++ src/App.js
@@ -2,21 +2,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // Named Import 
-import {Header} from './components/Header';
+import { Header } from './components/Header';
 // Default Import 
 import Body from './components/Body';
+import About from './components/About';
+import Contact from './components/Contact';
+import RestaurantMenu from './components/RestaurantMenu';
+import Error from './components/Error';
+//Routing configuration
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 
 
 const AppLayout = () => {
     return (
         <div className="App">
             <Header />
-            <Body />
+            <Outlet />
         </div>
     )
 
 }
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Body />,
+            },
+            {
+                path: "/about",
+                element: <About />,
+            },
+            {
+                path: "/contact",
+                element: <Contact />,
+            },
+            {
+                path: "/restaurants/:resId",
+                element: <RestaurantMenu />,
+            }
+        ],
+        errorElement: <Error />
+    },
 
+])
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />)
+root.render(<RouterProvider router={appRouter} />)
