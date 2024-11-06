ed82b90883f522b566b7e81e84a2e45e749ac726
Pragati <pragatikhard@gmail.com>
2024-11-06 19:00:56 +0530

    utils | mockData

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,6 +6,7 @@ export const CDN_URL =
 export const LOGO_URL =
     "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
 
+export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.6161&lng=73.7286&restaurantId="
 
 //  Can not write in such way, It will throw an error-
 //  export default CDN_URL;
