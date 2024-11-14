3cb1e991d6f9f2a0b2c78495ca34884eb805e948
Pragati <pragatikhard@gmail.com>
2024-11-15 00:26:22 +0530

    Components | Body

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,6 +3,7 @@ import resList from '../utils/mockdata';
 import { useState, useEffect } from 'react';
 import Shimmer from './Shimmer';
 import { Link } from 'react-router-dom';
+import useOnlineStatus from '../utils/useOnlineStatus';
 
 
 const Body = () => {
@@ -32,6 +33,15 @@ const Body = () => {
         setFilteredRestaurant(json?.data?.cards[1]?.card?.card.gridElements?.infoWithStyle?.restaurants)
 
     };
+    // use the custom hook here 
+    const onlineStatus = useOnlineStatus();
+
+    if (onlineStatus === false)
+        return (
+            <h1>
+                Looks like you're offline!! Please check your internet connection;
+            </h1>
+        );
 
     // Conditional rendering 
     // if (listOfRestaurants.length === 0) {
