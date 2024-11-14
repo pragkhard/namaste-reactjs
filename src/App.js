f2c6aa6e5ab1f22c89e40a72673f8396a3ac932f
Pragati <pragatikhard@gmail.com>
2024-11-15 00:26:02 +0530

    Swiggy App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 // Named Import 
 import { Header } from './components/Header';
 // Default Import 
 import Body from './components/Body';
-import About from './components/About';
+// import About from './components/About';
 import Contact from './components/Contact';
 import RestaurantMenu from './components/RestaurantMenu';
 import Error from './components/Error';
 //Routing configuration
 import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+//import Grocery from "./components/Grocery";
 
+// Chunking  Or
+// Code Splitting Or
+// Dynamic Bundling  Or
+// lazy Loading  Or
+// on demand loading  Or
+// dynamix imoprt
+
+const Grocery = lazy(() => import("./components/Grocery"));
+
+const About = lazy(() => import("./components/About"));
 
 const AppLayout = () => {
     return (
@@ -33,11 +44,17 @@ const appRouter = createBrowserRouter([
             },
             {
                 path: "/about",
-                element: <About />,
+                element: <Suspense fallback={<h1>Loading....</h1>}>
+                    <About />
+                </Suspense>,
             },
             {
                 path: "/contact",
                 element: <Contact />,
+            },
+            {
+                path: "/grocery",
+                element: <Suspense fallback={<h1>Loading......</h1>}><Grocery /></Suspense>,
             },
             {
                 path: "/restaurants/:resId",
